4ab727f0c64e907d77d386833fa094325151f66f
rayudu-web <64154612+rayudu-web@users.noreply.github.com>
2020-04-24 19:58:54 +0530

    Assertions

--- API Testing/Object Repository/GET List Users.rs
+++ API Testing/Object Repository/GET List Users.rs
@@ -18,5 +18,28 @@
    <soapHeader></soapHeader>
    <soapRequestMethod></soapRequestMethod>
    <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+import sun.util.logging.resources.logging
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+WSBuiltin
+
+
+
+
+</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
--- API Testing/Scripts/UserDefinedVerification/GET ListUsers DetailsVerification/Script1587636060048.groovy
+++ API Testing/Scripts/UserDefinedVerification/GET ListUsers DetailsVerification/Script1587636060048.groovy
@@ -1,20 +1,33 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-
-import groovy.json.JsonSlurper
+import groovy.json.JsonSlurper as JsonSlurper
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import internal.GlobalVariable as GlobalVariable
 
 //def jsonSlurper = new JsonSlurper()
 //String idValue = parsedJson.data.id
 //log.logInfo("idValue :" + idValue)
-
 Response = WS.sendRequest(findTestObject('GET List Users'))
 
 WS.verifyResponseStatusCode(Response, 200)
 
+//assertthat(Response.getStatusCode()).
 WS.verifyResponseStatusCodeInRange(Response, 150, 201)
 
 WS.verifyElementPropertyValue(Response, 'data[0].first_name', 'Michael')
 
 WS.verifyElementsCount(Response, 'data', 6)
 
+WS.getResponseStatusCode(Response)
